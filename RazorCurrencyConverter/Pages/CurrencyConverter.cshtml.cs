c4ed48e67fce1efb83d4d9d3bf328a8df087a190
amirmohamed <72021520+Amir94Mohamed@users.noreply.github.com>
2021-10-26 18:41:14 +0100

    initial

---
+++ RazorCurrencyConverter/Pages/CurrencyConverter.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc; 
+
+namespace RazorCurrencyConverter.Pages
+{
+    public class CurrencyConverterModel : PageModel
+    {
+
+        public const double GBP_TO_EURO = 1.18;
+
+        [BindProperty]
+        public double InputGBP { get; set; }
+        [BindProperty]
+        public double OutputEUR { get; set; }
+
+        public void OnGet()
+        {}
+         
+        public void OnPost()
+        {
+            if (double.IsNegative(InputGBP))
+            {
+                ViewData["Message"] = "Cannot enter a negative value!";
+            }
+            else
+            {
+                OutputEUR = InputGBP * GBP_TO_EURO;
+            }
+        }
+    }
+}
---
+++ RazorCurrencyConverter/Pages/Index.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorCurrencyConverter.Pages
+{
+    public class IndexModel : PageModel
+    {
+        [BindProperty]
+        public string FullName { get; set; }
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            FullName = "Amir Mohamed";
+        }
+        public void OnPost()
+        {
+            if (String.IsNullOrWhiteSpace(FullName))
+            {
+                ViewData["Message"] = "Name is missing!";
+                FullName = "Anonymous";
+            }
+            else
+            {
+                ViewData["Message"] = "Name is Registered!";
+            }
+        }
+    }
+}
